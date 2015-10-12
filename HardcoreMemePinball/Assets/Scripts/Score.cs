39f655d1163912b739daf17681c120071a8bc3f4
koedijk <17798@ma-web.nl>
2015-10-12 20:50:59 +0200

    Score Update
    Get/Set added + ToString voor op het scherm

--- HardcoreMemePinball/Assets/Scripts/Score.cs
+++ HardcoreMemePinball/Assets/Scripts/Score.cs
@@ -1,15 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Score : MonoBehaviour {
-
+    private int _Score;
+    private Text _ScoreText;
+    public int PlayerScore
+    {
+        get { return _Score; }
+        set { _Score = value; }
+    }
 	// Use this for initialization
 	void Start () {
-	
+        SetScoreText();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void SetScoreText()
+    {
+        _ScoreText.text = "Score: " + _Score.ToString();
+    }
+
 }
