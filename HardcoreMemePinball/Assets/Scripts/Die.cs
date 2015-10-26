3014d164d0e8bda90329da2a728a6ac93daaa8cc
Niels <Niels97vet@hotmail.com>
2015-10-26 09:39:58 +0100

    menus

---
+++ HardcoreMemePinball/Assets/Scripts/Die.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Die : MonoBehaviour {
+
+    void OnTriggerEnter(Collider col)
+    {
+        //Debug.Log (col.transform.tag);
+        if (col.gameObject.tag == "Player")
+        {
+            Application.LoadLevel("LoseScreen");
+            //Debug.Log(col.gameObject.tag == "Player");
+        }
+    }
+}
