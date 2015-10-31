c44916ba37e46169f87127adb91a357971f8d1f1
koedijk <17798@ma-web.nl>
2015-10-31 01:54:42 +0100

    Update

--- HardcoreMemePinball/Assets/Scripts/PickUp.cs
+++ HardcoreMemePinball/Assets/Scripts/PickUp.cs
@@ -6,17 +6,18 @@ public class PickUp : MonoBehaviour {
 	private GameObject _player;
 	[SerializeField]
 	private GameObject _PickUpManager;
-	private float _scale = 1.25f;
+	private float _scale = 0.25f;
 
 	void OnTriggerEnter(Collider other)
 	{
 
 		if (other.gameObject.tag == "Player") 
 		{
+			Debug.Log("Played1");
 			if(this.gameObject.tag == "Scale")
 			{	
 				this.gameObject.transform.position -= new Vector3(0,2,0);
-				_player.transform.localScale = new Vector3 (_scale, _scale, _scale);
+				_player.transform.localScale += new Vector3 (_scale, _scale, _scale);
 		   		StartCoroutine(PickUpScaleTimeOut());
 			}
 			if(this.gameObject.tag == "Multiply")
@@ -31,16 +32,16 @@ public class PickUp : MonoBehaviour {
 
 	IEnumerator PickUpScaleTimeOut()
 	{
-		yield return new WaitForSeconds(5);
-		_player.transform.localScale = new Vector3(1,1,1);
-		this.gameObject.transform.position = new Vector3(0,0,0);
+		yield return new WaitForSeconds(15);
+		_player.transform.localScale -= new Vector3(_scale,_scale,_scale);
+		//this.gameObject.transform.position = new Vector3(0,0,0);
 
 	}
 	IEnumerator PickUpMultiplyTimeOut()
 	{
-		yield return new WaitForSeconds(10);
+		yield return new WaitForSeconds(15);
 		Destroy(GameObject.Find("Player(Clone)"));
-		this.gameObject.transform.position = new Vector3(0,0,0);
+		//this.gameObject.transform.position = new Vector3(0,0,0);
 		
 	}
 }
